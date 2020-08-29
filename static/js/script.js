9cd03a816a139e2c6564574a3aade79d1dcde7a7
muhammadyusronn <muhammadyusron733@gmail.com>
2020-08-29 16:54:57 +0700

    Menambahkan file javascript ke dalam challenge Blackjack

--- static/js/script.js
+++ static/js/script.js
@@ -161,6 +161,51 @@ function buttonsRandom(){
 	}
 }
 
+// Challenge 5 : Blackjack
+let blackjackGame = {
+	'you' : {'scoreSpan' : '#your-blackjack-result', 'div' : '#your-box', 'score' : 0},
+	'dealer' : {'scoreSpan' : '#dealer-blackjack-result', 'div' : '#dealer-box', 'score' : 0},
+	'cards' : ['2','3','4','5','6','7','8','9','10','A','K','J','Q'],
+}
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+var hitSound = new Audio('static/sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackHit(){
+	let card = randomCard();
+	showCard(card,YOU);
+}
+
+function randomCard(){
+	let randomIndex = Math.floor(Math.random() * 13);
+	return blackjackGame['cards'][randomIndex]; 
+}
+
+function showCard(card,activePlayer){
+	// alert('Ouch, you clicked me!');
+	let cardImage = document.createElement('img');
+	cardImage.src=`static/images/${card}.png`;
+	document.querySelector(activePlayer['div']).appendChild(cardImage);
+	hitSound.play();
+}
+
+function blackjackDeal(){
+	let yourImage = document.querySelector('#your-box').querySelectorAll('img');
+	let dealerImage = document.querySelector('#dealer-box').querySelectorAll('img');
+	
+	for(let i = 0; i<yourImage.length; i++){
+		yourImage[i].remove();
+	}
+
+	for(let i = 0; i<dealerImage.length; i++){
+		dealerImage[i].remove();
+	}
+}
+
 
 
 
